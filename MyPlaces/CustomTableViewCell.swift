742cead7b09cdd29f433ac7360fc1c4d176bebc0
akakoulin <alexey1312@me.com>
2019-08-22 16:03:43 +0700

    add custom table view cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Admin on 22/08/2019.
+//  Copyright © 2019 Aleksei Kakoulin. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
